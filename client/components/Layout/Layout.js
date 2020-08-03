178dec9e49fc15517b86f7dbcc0518b9800904dc
ekremtas <ekremtas589@gmail.com>
2020-08-03 13:46:41 +0300

    Checking the "auth" cookie on the layout page, determining the bar function.

--- client/components/Layout/Layout.js
+++ client/components/Layout/Layout.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import { Bar, Footer } from "../../components";
+import Cookies from 'js-cookie';
 
 const Layout = ({ children, auth, authPage, logOut }) => {
+
+  const [cookieAuth, setCookieAuth] = useState(false);
+  // auth information control from cookies , for bar 
+  useEffect(()=>{
+    setCookieAuth(Cookies.get('auth') ? true : false);
+  },[])
+
   return (
     <>
-      <Bar auth={auth} logOut={logOut} authPage={authPage} />
+      {/* (auth value) allows him to check the auth value assigned to the pages before for now */}
+      <Bar auth={cookieAuth?cookieAuth:auth} logOut={logOut} authPage={authPage} />
       {children}
       <Footer />
     </>
