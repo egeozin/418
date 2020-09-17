3101c8948384e1fa792760ca9b3e256db7ec4402
Sinan Işık <sinan.isik@ug.bilkent.edu.tr>
2020-09-17 20:47:53 +0300

    handlePastedText

--- client/components/Editor/EditorArea/BetterEditor.js
+++ client/components/Editor/EditorArea/BetterEditor.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import MUIRichTextEditor from "mui-rte";
-import { convertToRaw } from "draft-js";
+import { ContentState, convertToRaw, EditorState, Modifier } from "draft-js";
 import { makeStyles } from "@material-ui/core/styles";
 
 import CodeIcon from "@material-ui/icons/Code";
 import CodeBlock from "./CustomBlocks/CodeBlock";
 import FormatQuoteIcon from "@material-ui/icons/FormatQuote";
 import Blockquote from "./CustomBlocks/Blockquote";
+import CodeUtils from "draft-js-code";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,11 +20,61 @@ const BetterEditor = (props) => {
   const [loading, setLoading] = useState(false);
   const { forwardRef, label } = props;
 
+  const handlePastedText = (text, html, editorState) => {
+    props.handleChange(
+      "bodyText",
+      convertToRaw(editorState.getCurrentContent())
+    );
+
+    const selection = editorState.getSelection();
+    const pastedBlocks = ContentState.createFromText(text).blockMap;
+    const newState = Modifier.replaceWithFragment(
+      editorState.getCurrentContent(),
+      editorState.getSelection(),
+      pastedBlocks
+    );
+    const newEditorState = EditorState.push(
+      editorState,
+      newState,
+      "insert-fragment"
+    );
+    console.log(
+      "handlePastedText -> newEditorState",
+      convertToRaw(newEditorState.getCurrentContent())
+    );
+
+    props.handleChange(
+      "bodyText",
+      convertToRaw(newEditorState.getCurrentContent())
+    );
+
+    return "not-handled";
+  };
+
+  const hasSelectionInCustomBlock = (editorState) => {
+    let selection = editorState.getSelection();
+    let contentState = editorState.getCurrentContent();
+    let startKey = selection.getStartKey();
+    let currentBlock = contentState.getBlockForKey(startKey);
+    return currentBlock.getType() === "CODEBLOCK";
+  };
+
+  const onTab = (evt) => {
+    // if (!CodeUtils.hasSelectionInBlock(editorState)) return "not-handled";
+
+    // this.onChange(CodeUtils.onTab(evt, editorState));
+    // return "handled";
+    if (!CodeUtils.hasSelectionInBlock(editorState)) {
+      console.log("handled");
+    }
+  };
+
   const onChange = (editorState) => {
     props.handleChange(
       "bodyText",
       convertToRaw(editorState.getCurrentContent())
     );
+    // console.log(hasSelectionInCustomBlock(editorState));
   };
 
   const onBlur = () => {
@@ -38,7 +89,7 @@ const BetterEditor = (props) => {
     <MUIRichTextEditor
       label={label}
       ref={forwardRef}
-      onChange={onChange}
+      // onChange={onChange}
       onBlur={onBlur}
       className={classes.root}
       controls={[
@@ -51,6 +102,7 @@ const BetterEditor = (props) => {
         "Blockquote",
         "codeBlock",
         "clear",
+        "code",
       ]}
       customControls={[
         {
@@ -66,6 +118,10 @@ const BetterEditor = (props) => {
           blockWrapper: <Blockquote />,
         },
       ]}
+      onTab={onTab}
+      draftEditorProps={{
+        handlePastedText: handlePastedText,
+      }}
     />
   );
 };
