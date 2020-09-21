f823385e817bdc657b3c40ae4307bce1c4358878
Ege Ozgirin <ege@Eges-MacBook-Air.local>
2020-09-21 19:10:17 -0400

    added kvkk forms

--- client/pages/auth/[[...path]]/index.js
+++ client/pages/auth/[[...path]]/index.js
@@ -1,22 +1,49 @@
 import FirebaseAuth from "../../../components/FirebaseAuth";
+import { useState } from "react";
 import { useRouter } from 'next/router'
 import { makeStyles, Grid } from "@material-ui/core";
 import { Layout } from "../../../components";
+import Consent from "../../../components/Consent";
 
 const useStyles = makeStyles((theme) => ({
   authContainer: {
     marginTop: 20,
   },
+  consentContainer: {
+  }
 }));
 
 const Auth = () => {
   const classes = useStyles()
+  const [signedPolicy, setSignedPolicy] = useState(false);
+
+  const giveConsent = (e) => {
+    e.preventDefault()
+    setSignedPolicy(true)
+  }
+
   return (
     <Layout authPage={true}>
       <Grid container spacing={4} className={classes.authContainer}>
-        <Grid item xs={12} md={12}>
-          <FirebaseAuth />
-        </Grid>
+        {signedPolicy ?
+          <Grid item xs={12} md={12}>
+            <FirebaseAuth />
+          </Grid>
+          :
+          <>
+          <Grid item xs={12} md={3} />
+          <Grid 
+            item 
+            direction="column"
+            style={{textAlign: "center"}}
+            xs={12} 
+            md={6} 
+          >
+              <Consent handleClick={giveConsent} />
+          </Grid>
+          <Grid item xs={12} md={3} />
+          </>
+        }
       </Grid>
     </Layout>
   );
