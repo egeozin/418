b400619267e3fb2d5fcf3a3bc5ff6886bdba202d
Ege Ozgirin <ege@Eges-MacBook-Air.local>
2020-09-26 08:54:45 -0400

    added languages

--- client/src/languages.js
+++ client/src/languages.js
@@ -5,13 +5,15 @@ const languages = [
   { name: "C++", color: "#FF7BAC" },
   { name: "Java", color: "#b2475d" },
   { name: "React", color: "#15A160" },
+  { name: "HTML", color: "#E5CE37" },
+  { name: "CSS", color: "#EB6D64" },
   { name: "JavaScript", color: "#F5B50F" },
   { name: "p5js", color: "#FF7D75" },
   { name: "Go", color: "#2969DE" },
   { name: "Kotlin", color: "#f9d371" },
   { name: "Rust", color: "#eb7f64" },
   { name: "Pascal", color: "#eb7f64" },
-  { name: "C#", color: "#8A2BE2" },
+  { name: "C#", color: "#A45F92" },
 ];
 
 export default languages;
