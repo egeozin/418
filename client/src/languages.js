8adbe44ac336bdceb70e0857501194fcc024e8e6
Ege Ozgirin <ege@Eges-MacBook-Air.local>
2020-09-28 09:36:02 -0400

    added php, laravel

--- client/src/languages.js
+++ client/src/languages.js
@@ -8,6 +8,8 @@ const languages = [
   { name: "HTML", color: "#E5CE37" },
   { name: "CSS", color: "#EB6D64" },
   { name: "JavaScript", color: "#F5B50F" },
+  { name: "PHP", color: "#77AA7B"},
+  { name: "Laravel", color: "#246282"},
   { name: "p5js", color: "#FF7D75" },
   { name: "Go", color: "#2969DE" },
   { name: "Kotlin", color: "#f9d371" },
